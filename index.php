5e2eb3c78092d6df4f28a0684f54eb4ef94949a6
Henrik Brix Andersen <brix@src.gnome.org>
2002-09-19 14:06:57 +0000

    Initial revision

---
+++ index.php
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <title>GIMP - The GNU Image Manipulation Program</title>
+    <link rel="stylesheet" type="text/css" href="style/default.css"/>
+  </head>
+ 
+  <body>
+
+    <div id="titlebar">
+      <span id="titlebarleft">
+        <img src="images/thegimp.png" alt=""/>
+      </span>
+      <span id="titlebarright">
+        <img src="images/wilberright.png" alt=""/>    
+      </span>
+      <img src="images/barbg.png" alt=""/>
+    </div>
+
+    <table id="layout">
+      <tr>
+        <td id="menu">
+          <a href="?page=news">News</a><br/>
+          <a href="?page=about">About GIMP</a><br/>
+          <a href="?page=resources">Resources</a><br/>
+          <a href="?page=registry">Plug-In Registry</a><br/>
+          <a href="?page=doc">Documentation</a><br/>
+          <a href="?page=tutorials">Tutorials</a><br/>
+          <a href="?page=script">Script Authoring</a><br/>
+          <a href="?page=devel">Development</a><br/>
+          <br/>
+          <a href="?page=unix">GIMP for Unix</a><br/>
+          <a href="?page=win32">GIMP for Windows</a><br/>
+          <a href="?page=mac">GIMP for Macintosh</a><br/>
+          <br/>
+          Newest Plug-Ins:<br/>
+          <a href="1">Gaussian blur * (1.0)</a><br/>
+          <a href="1">PSD Import/Export (2.7)</a><br/>
+        </td>
+
+        <td id="main">
+          <div class="heading">
+            <span class="title">
+              Hello World
+            </span>
+            <span class="date">
+              2002-09-17
+            </span>
+            &nbsp;
+          </div>
+          <img src="images/nicefolder.png" alt="" class="icon"/>
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item,
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+          Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, Some boring news item, 
+        </td>
+      </tr>
+    </table>
+    
+    <div id="linkbar">
+      GIMP Links :
+      <span class="linkitem"><a href="1">GIMP User Group</a></span>
+      <span class="linkitem"><a href="1">Grokking With the GIMP</a></span>
+      <span class="linkitem"><a href="1">Mailing Lists</a></span>
+      <span class="linkitem"><a href="1">FAQ's</a></span>
+      <span class="linkitem"><a href="1">GIMP Books</a></span>
+    </div>
+
+    <div>
+      <span id="footerleft">
+        &copy; 2001-2002 The GIMP Team
+      </span>
+      <span id="footerright">
+        <a href="mailto:webmaster@gimp.org">webmaster@gimp.org</a>
+      </span>
+    </div>
+
+  </body>
+</html>

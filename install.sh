76c803bd633207f5ca66b62ec14e6db93179d7a7
Helvetix Victorinox <Helvetix@src.gnome.org>
2003-03-06 04:55:42 +0000

    *** empty log message ***

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ if test -z "$PYTHON" ; then
   PYTHON=python
 fi
 
-make PYTHON=${PYTHON} DocumentRoot=${INSTALLDIR} clean all install 2>&1 > make.out
+make PYTHON=${PYTHON} DocumentRoot=${INSTALLDIR} clean all install 2>&1 > ${INSTALLDIR}/make.out
 
 rsync -rlt --delete --exclude-from=$CVSDIR/install.exclude $CVSDIR/ $INSTALLDIR/
 
-(cd programmatic ; make PYTHON=${PYTHON} DocumentRoot=${INSTALLDIR} all install )
+(cd programmatic ; make PYTHON=${PYTHON} DocumentRoot=${INSTALLDIR} all install ) 2>&1 >> ${INSTALLDIR}/make.out
 
-(cd crontab ; make all install)
+(cd crontab ; make all install) 2>&1 >> ${INSTALLDIR}/make.out

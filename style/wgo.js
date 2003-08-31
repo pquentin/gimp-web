6c649dfe66f19c6aff6fcfb3a08d883fe47e3d1e
Raphael Quinet <quinet@gamers.org>
2003-08-31 20:10:23 +0000

    New style sheet, containing most of what was in default.css (only the
    2003-08-31  Raphael Quinet  <quinet@gamers.org>
    
    	* style/wgo.css: New style sheet, containing most of what was in
    	default.css (only the classes that were used somewhere).
    
    	* style/wgo.js: New file containing some bits of JavaScript for
    	adjusting the size of the navigation menu and for toggling the
    	visibility of that menu.
    
    	* images/menubutton.png: New image for the corner of the menu.
    
    	* includes/wgo-xhtml-init.xhtml
    	* includes/wgo-xhtml-fini.xhtml
    	* includes/wgo-page-init.xhtml
    	* includes/wgo-page-fini.xhtml
    	* includes/wgo-styles.xhtml: Updated for new page layout.
    
    	* includes/footer.inc
    	* includes/header_posttitle.inc
    	* includes/header_pretitle.inc
    	* includes/linkbar.inc
    	* includes/menu.inc
    	* includes/tutnavbar.inc
    	* includes/urls_maillist.inc
    	* includes/wgo-menu.xhtml: Deleted these unused files.  Some of
    	them are still used by the old files in about/Changelogs/* but
    	those files should be deleted and replaced by something else so it
    	is not a big problem if they are temporarily broken.
    
    	* docs/index.htrw,
    	* tutorials/Smart_Sharpening/warp-sharp.htrw,
    	* tutorials/Lite_Quickies/index.htrw,
    	* tutorials/Blur_Overlays/index.htrw: <div class="subtitle"> is
    	replaced by <h2>.  The class "subtitle" is now gone.

---
+++ style/wgo.js
@@ -0,0 +1,56 @@
+// JavaScript code for www.gimp.org
+// $Id$
+
+var main_el;
+var menu_el;
+var button_el;
+var menu_shown = true;
+
+if (document.getElementById) {
+  // W3C DOM
+  main_el = document.getElementById("main");
+  menu_el = document.getElementById("menu");
+  button_el = document.getElementById("menubutton");
+} else if (document.all) {
+  // IE4 DOM
+  main_el = document.all["main"];
+  menu_el = document.all["menu"];
+  button_el = document.all["menubutton"];
+}
+
+function adjust_menu () {
+  var padding = 10;
+  if (menu_el && main_el) {
+    if (main_el.offsetHeight) {
+      menu_el.style.height = (main_el.offsetHeight - 2 * padding) + "px";
+    }
+    if (menu_el.offsetWidth) {
+      main_el.style.marginLeft = (menu_el.offsetWidth + padding) + "px";
+    }
+  }
+}
+
+function toggle_menu () {
+  if (menu_shown) {
+    if (menu_el) {
+      menu_el.style.display = "none";
+    }
+    if (main_el) {
+      main_el.style.marginLeft = "0px";
+    }
+    menu_shown = false;
+  } else {
+    if (menu_el) {
+      menu_el.style.display = "block";
+    }
+    menu_shown = true;
+    adjust_menu ();
+    if (button_el) {
+      // Small hack because zIndex does not always work
+      button_el.style.display = "none";
+      button_el.style.display = "block";
+    }
+  }
+}
+
+adjust_menu ();

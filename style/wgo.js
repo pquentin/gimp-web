539c61d8f75d2e299d00fac3e5ed336b59ce266a
Raphael Quinet <quinet@gamers.org>
2003-09-01 11:41:39 +0000

    includes/wgo-page-init.xhtml includes/wgo-page-fini.xhtml
    2003-09-01  Raphael Quinet  <quinet@gamers.org>
    
    	* includes/wgo-page-init.xhtml
    	* includes/wgo-page-fini.xhtml
    	* includes/wgo-xhtml-fini.xhtml
    	* style/wgo.js: Include the menu button only if the browser
    	supports JavaScript and W3C DOM or IE4 DOM.  The corresponding div
    	and image are now inserted by JavaScript.  Re-organized the code a
    	bit in order to make it easier to read and to allow pages in which
    	the menu is hidden by default.  This could be useful for tutorials
    	and howtos.

--- style/wgo.js
+++ style/wgo.js
@@ -1,4 +1,4 @@
-// JavaScript code for www.gimp.org
+// JavaScript code for www.gimp.org - Copyright (C) 2003, Raphael Quinet
 // $Id$
 
 var main_el;
@@ -6,19 +6,7 @@ var menu_el;
 var button_el;
 var menu_shown = true;
 
-if (document.getElementById) {
-  // W3C DOM
-  main_el = document.getElementById("main");
-  menu_el = document.getElementById("menu");
-  button_el = document.getElementById("menubutton");
-} else if (document.all) {
-  // IE4 DOM
-  main_el = document.all["main"];
-  menu_el = document.all["menu"];
-  button_el = document.all["menubutton"];
-}
-
-function adjust_menu () {
+function menu_adjust () {
   var padding = 10;
   if (menu_el && main_el) {
     if (main_el.offsetHeight && menu_el.offsetHeight) {
@@ -34,27 +22,50 @@ function adjust_menu () {
   }
 }
 
-function toggle_menu () {
-  if (menu_shown) {
-    if (menu_el) {
-      menu_el.style.display = "none";
-    }
-    if (main_el) {
-      main_el.style.marginLeft = "0px";
-    }
-    menu_shown = false;
-  } else {
-    if (menu_el) {
-      menu_el.style.display = "block";
-    }
-    menu_shown = true;
-    adjust_menu ();
-    if (button_el) {
-      // Small hack because zIndex does not always work
-      button_el.style.display = "none";
-      button_el.style.display = "block";
-    }
+function menu_hide () {
+  if (menu_el) {
+    menu_el.style.display = "none";
+  }
+  if (main_el) {
+    main_el.style.marginLeft = "0px";
+  }
+  menu_shown = false;
+}
+
+function menu_show () {
+  if (menu_el) {
+    menu_el.style.display = "block";
+  }
+  menu_shown = true;
+  menu_adjust ();
+  if (button_el) {
+    // Small hack because zIndex does not work in all cases
+    button_el.style.display = "none";
+    button_el.style.display = "block";
   }
 }
 
+function menu_toggle () {
+  if (menu_shown) {
+    menu_hide ();
+  } else {
+    menu_show ();
+  }
+}
+
+// Initialize the variables and add the button for toggling the menu
+if (document.getElementById) {
+  // W3C DOM
+  main_el = document.getElementById("main");
+  menu_el = document.getElementById("menu");
+  button_el = document.getElementById("menubutton");
+} else if (document.all) {
+  // IE4 DOM
+  main_el = document.all["main"];
+  menu_el = document.all["menu"];
+  button_el = document.all["menubutton"];
+}
 adjust_menu ();
+if (menu_el) {
+  document.write('<div id="menubutton"><img src="/images/menubutton.png" alt="" border="0" onclick="menu_toggle();" /></div>');
+}

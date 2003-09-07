a1fd23ff8b473c3e5c201be7f5e263581c13c6c8
Helvetix Victorinox <Helvetix@src.gnome.org>
2003-09-07 21:03:15 +0000

    Fixed typo New -> News.
    * programmatic/admin/news/howto-news.html: Fixed typo New -> News.

--- programmatic/contest/wgo_contest.py
+++ programmatic/contest/wgo_contest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env ${PYTHON}
 # -*- mode: python py-indent-offset: 2; -*-
+# $Id$
 #
 # www.gimp.org website administration and tools
 # Copyright (C) 2002, 2003  Helvetix Victorinox, a pseudonym

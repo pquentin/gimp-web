20f52a4cd5c7120961720b87d161ce4dc648d60b
Sven Neumann <sven@gimp.org>
2007-11-30 11:29:16 +0000

    removed directory full of outdated scripts.
    2007-11-30  Sven Neumann  <sven@gimp.org>
    
    	* programmatic/about: removed directory full of outdated scripts.
    
    	* programmatic/Makefile: removed targets for about.
    
    svn path=/trunk/; revision=1745

--- programmatic/about/ChangeLogs/changelog-cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-${DocumentRoot}/about/ChangeLogs/film-gimp
-${DocumentRoot}/about/ChangeLogs/gegl-unstable
-${DocumentRoot}/about/ChangeLogs/gimp-help
-${DocumentRoot}/about/ChangeLogs/gimp-print
-${DocumentRoot}/about/ChangeLogs/gimp-unstable
-
-exit 0

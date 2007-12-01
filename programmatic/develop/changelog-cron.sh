870e19db967d76f91cdfaddf7630bc1402e4d3cd
Sven Neumann <sven@gimp.org>
2007-12-01 12:04:22 +0000

    programmatic/Makefile removed unused directory.
    2007-12-01  Sven Neumann  <sven@gimp.org>
    
    	* programmatic/Makefile
    	* programmatic/develop: removed unused directory.
    
    	* programmatic/admin/Makefile
    	* programmatic/admin/develop: removed unused directory.
    
    
    svn path=/trunk/; revision=1752

--- programmatic/develop/changelog-cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-${DocumentRoot}/develop/film-gimp
-${DocumentRoot}/develop/gegl-unstable
-${DocumentRoot}/develop/gimp-help
-${DocumentRoot}/develop/gimp-print
-${DocumentRoot}/develop/gimp-unstable
-
-exit 0
